f986b8ea75bbce94dade775eeccea55c8afdbfa1
hdy <lldgll@naver.com>
2024-01-18 18:01:42 +0900

    [Feat] DrawRay
    레이캐스트 보이게

--- Assets/3.Script/Player/PlayerRayCheck.cs
+++ Assets/3.Script/Player/PlayerRayCheck.cs
@@ -46,17 +46,23 @@ public class PlayerRayCheck : MonoBehaviour
         //RaycastHit2D boxHit = Physics2D.BoxCast(groundCheck.position, boxSize, 0, Vector2.down, 0, Ground_and_Wall);
 
         Debug.DrawRay(wallCheck.position, (Vector2.right * pCon.isRightInt) * wallRayLength, Color.red);
+        
         isCeiling = Physics2D.Raycast(transform.position, Vector2.up, rayCeilLength, groundLayer);
         isSloth = frontSloth || backSloth;
         isWall = Physics2D.Raycast(wallCheck.position, Vector2.right * pCon.isRightInt, wallRayLength, wallLayer);
         isCanJump = lineHit;
         //isGround = lineHit;
-
     }
 
     private void OnDrawGizmos()
     {
+        Gizmos.color = Color.red;
         Gizmos.DrawWireCube(groundCheck.position, boxSize);
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(transform.position, Vector2.up * rayCeilLength);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(transform.position, Vector2.down * rayGroundLength);
+
     }
 
 }
